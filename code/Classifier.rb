ec598e6f02244578edeb10bd32b0a46c8bc075dd
Ichikawa Yuya <ichikun.cocoa+github@gmail.com>
2015-09-11 13:43:07 +0900

    Update Classifier.rb

--- code/Classifier.rb
+++ code/Classifier.rb
@@ -1,59 +1,41 @@
 #!/usr/bin/env ruby
 # -*- coding: utf-8 -*-
 require "./MachineLearning.rb"
+#require "./GraduateThesis.rb"
 
 
 class Classifier < MachineLearning
         @predict_data
         @label
+        @train_data
 
-        def intilize(host, port, name)
+        def initialize(host, port, name)
                 JubatusServerConnect(host, port, name)
         end
         def JubatusServerConnect(host, port, name)
                  @client = Jubatus::Classifier::Client::Classifier.new(host, port, name)
         end
-        def GetLabel()
-                #return @labbel
-                @label = @client.get_labels()
-        end
         #教師あり学習させる
-        def Train(label, key, values)
-                #すべての配列にlabelを格納
-                train =  Array.new(values.length).map{Array.new(2, "#{label}")}
-                count = 0
-                values.each do |content|
-                        train[count][1] =  getDataConstruct(key, content)
-
-                        count = count + 1
-                end
-                train.sort_by{rand}
-                @client.train(train)
+        def Train(label, key_values)
+                @train_data = KeyValues(key_values)
+                @train_data =[["#{label}", getDataConstruct(@train_data)],]
+                @client.train(@train_data)
         end
 
         #教師あり学習から予測する
-        def Predict(key, values)
-                data = []
-                get_label = []
-
+        def Classify(key_values)
                 #valuesの値をJubatusのデータ構造に変換し格納する
-                values.each do |value|
-                         data.concat([getDataConstruct(key,value)])
-                end
+                predict_data = KeyValues(key_values)
 
-                data.each do |predict_data|
-                        result = @client.classify([predict_data])
-                        get_label.concat([result[0].max_by{|x| x.score}.label])
-                end
-
-                @label = get_label
+                result = @client.classify([getDataConstruct(predict_data)])
+                return result
         end
 
 
 
         #学習しているすべてのラベルを返す
-        def getLabel()
-                #return @client.get_labels()
+        def getLabels()
+                @label = @client.get_labels()
                 return @label
         end
 
